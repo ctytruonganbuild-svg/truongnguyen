4942f05e7a23e6626fdf912614b45feecaf3b5e9
ctytruonganbuild-svg <ctytruonganbuild@gmail.com>
2026-04-19 09:41:19 +0700

    nop

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import QuadraticSolver from './components/QuadraticSolver';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-50 to-blue-100 py-12 flex flex-col items-center justify-center font-sans antialiased text-gray-900">
+      <QuadraticSolver />
+    </div>
+  );
+}
